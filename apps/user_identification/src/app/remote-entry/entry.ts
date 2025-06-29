ac7e63f1f8e3b9fb67109cc024b22f0b74a39e46
mesquitaigor <igor.mesqbessa@gmail.com>
2025-06-29 14:41:12 -0300

    fix(user_identification): add Vercel configuration

--- apps/user_identification/src/app/remote-entry/entry.ts
+++ apps/user_identification/src/app/remote-entry/entry.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
 import { NxWelcome } from './nx-welcome';
+
 
 @Component({
   imports: [CommonModule, NxWelcome],
-  selector: 'app-user_identification-entry',
+  selector: 'app-user-identification-entry',
   template: `<app-nx-welcome></app-nx-welcome>`,
 })
 export class RemoteEntry {}
