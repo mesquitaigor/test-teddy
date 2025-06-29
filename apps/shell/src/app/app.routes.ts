a784249ab23ba9d73f2885799f2af476792a7271
mesquitaigor <igor.mesqbessa@gmail.com>
2025-06-29 20:12:47 -0300

    feat: create identification guard

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Route } from '@angular/router';
+import { identificationGuard } from './shared/guards/identification-guard';
 
 export const appRoutes: Route[] = [
   {
     path: 'clients',
     loadChildren: () => import('clients/Routes').then((m) => m!.remoteRoutes),
+    canActivate: [identificationGuard]
   },
   {
     path: '',
---
+++ apps/shell/src/app/shared/guards/identification-guard.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { CanActivateFn } from '@angular/router';
+
+import { identificationGuard } from './identification-guard';
+
+describe('identificationGuard', () => {
+  const executeGuard: CanActivateFn = (...guardParameters) => 
+      TestBed.runInInjectionContext(() => identificationGuard(...guardParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeGuard).toBeTruthy();
+  });
+});
