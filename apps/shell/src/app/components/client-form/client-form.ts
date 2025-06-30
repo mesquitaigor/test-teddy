d1c46882649acbfad53915585fad891091ecdd09
mesquitaigor <igor.mesqbessa@gmail.com>
2025-06-30 14:25:37 -0300

    chore: add Maskito packages and input mask

--- apps/shell/src/app/components/client-form/client-form.ts
+++ apps/shell/src/app/components/client-form/client-form.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { maskitoNumberOptionsGenerator } from '@maskito/kit';
 import { TeddyButtonComponent } from '@teddy/components';
+import { MaskitoDirective } from '@maskito/angular';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-client-form',
-  imports: [CommonModule, FormsModule, TeddyButtonComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TeddyButtonComponent,
+    MaskitoDirective,
+    ReactiveFormsModule,
+  ],
   templateUrl: './client-form.html',
   styleUrl: './client-form.scss',
 })
-export class ClientForm {}
+export class ClientForm {
+  readonly control = new FormGroup({
+    salaryInput: new FormControl(''),
+    companyValuationInput: new FormControl(''),
+  });
+  moneyMask = maskitoNumberOptionsGenerator({
+    decimalSeparator: ',', // separador decimal brasileiro
+    thousandSeparator: '.', // separador de milhar brasileiro
+    precision: 2, // duas casas decimais
+    prefix: 'R$ ', // prefixo monetário
+  });
+}
